d7e0090b58d0163f9a9e1da9060abaf9be688b2d
hanslelee <hssarah13@gmail.com>
2022-06-30 21:23:46 +0900

    Initial commit

---
+++ src/main/java/com/apebble/askwatson/AskWatsonApplication.java
@@ -0,0 +1,13 @@
+package com.apebble.askwatson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskWatsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskWatsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apebble/askwatson/AskWatsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apebble.askwatson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskWatsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
