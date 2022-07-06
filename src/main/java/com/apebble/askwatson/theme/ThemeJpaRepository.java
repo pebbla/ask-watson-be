84796ba204dc27e5b82ebf6d5ec33d3f8071cb15
hanslelee <hssarah13@gmail.com>
2022-07-06 12:51:44 +0900

    theme api - getAllThemes, getTheme, getThemesByCafe, modifyTheme, deleteTheme

--- src/main/java/com/apebble/askwatson/theme/ThemeJpaRepository.java
+++ src/main/java/com/apebble/askwatson/theme/ThemeJpaRepository.java
@@ -1,6 +1,10 @@
 package com.apebble.askwatson.theme;
 
+import com.apebble.askwatson.cafe.Cafe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ThemeJpaRepository extends JpaRepository<Theme, Long> {
+    List<Theme> findThemesByCafe(Cafe cafe);
 }
