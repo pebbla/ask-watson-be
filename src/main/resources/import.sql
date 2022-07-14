61c853e6183efd8fbf36ce97286e27290fc30f28
hanslelee <hssarah13@gmail.com>
2022-07-14 22:31:11 +0900

    import.sql 수정 - user, review 데이터 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -47,8 +47,13 @@ INSERT INTO category(category_name) VALUES ('기타');
 INSERT INTO admin(admin_account,admin_name,admin_password) VALUES ('hssarah','이한슬','qwer1234');
 INSERT INTO admin(admin_account,admin_name,admin_password) VALUES ('arock1998','최아록','qwer1234');
 
+INSERT INTO user(user_password,user_nickname,user_phone_num,user_birth,user_gender,marketing_agree_yn) VALUES ('qwer', '우영우', '010-1511-2662', '1995/04/02', '0', 0);
+INSERT INTO user(user_password,user_nickname,user_phone_num,user_birth,user_gender,marketing_agree_yn) VALUES ('1234', '홍길동', '010-1111-2222', '1900/01/01', '1', 0);
+
 INSERT INTO cafe(cafe_name,cafe_phone_num,location_id,company_id) VALUES ('포인트나인 강남점','02-1919-1919',1, 1);
 INSERT INTO cafe(cafe_name,cafe_phone_num,location_id,company_id) VALUES ('방탈출고고싱','02-2020-1124',2, 4);
 
 INSERT INTO theme(theme_name,theme_explanation,category_id,difficulty,time_limit,cafe_id) VALUES('포인트나인나인','포인트 아홉개를 연결함으로써 별자리를 완성해보세요!',3,3.5,60,1);
 INSERT INTO theme(theme_name,theme_explanation,category_id,difficulty,time_limit,cafe_id) VALUES('포인트에잇','포인트 여덟개를 연결해서 별자리를 완성해봅시다',1,4.0,70,1);
+
+INSERT INTO review(rating,content,theme_id) VALUES (3.5, '리뷰입니다', 1);
