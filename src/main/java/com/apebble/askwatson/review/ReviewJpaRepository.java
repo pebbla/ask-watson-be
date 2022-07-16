e06c0b3f1046c0e00103a903073fb98ee3a01e09
arock <arock1998@naver.com>
2022-07-16 13:00:55 +0900

    feat: 리뷰 등록, 유저별 리뷰 조회, 테마별 리뷰 조회, 리뷰 수정, 리뷰 삭제

--- src/main/java/com/apebble/askwatson/review/ReviewJpaRepository.java
+++ src/main/java/com/apebble/askwatson/review/ReviewJpaRepository.java
@@ -1,6 +1,14 @@
 package com.apebble.askwatson.review;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.apebble.askwatson.user.User;
+
 public interface ReviewJpaRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByUser(User user);
+    List<Review> findByThemeId(Long themeId);
+
 }
