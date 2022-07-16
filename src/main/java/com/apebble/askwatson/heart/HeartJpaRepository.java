03a28d6feb1f478c4d251e1cb551f63f57306f01
arock <arock1998@naver.com>
2022-07-16 21:36:04 +0900

    feat: test test

--- src/main/java/com/apebble/askwatson/heart/HeartJpaRepository.java
+++ src/main/java/com/apebble/askwatson/heart/HeartJpaRepository.java
@@ -3,6 +3,8 @@ package com.apebble.askwatson.heart;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.apebble.askwatson.user.User;
+
 public interface HeartJpaRepository extends JpaRepository<Heart, Long> {
 
     List<Heart> findByUserId(Long userId);
