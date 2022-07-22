2950f6269c9add3b8ef034feaaee33b5934f0fdb
arock <arock1998@naver.com>
2022-07-23 04:27:50 +0900

    feat: 스웨거 적용

--- src/main/java/com/apebble/askwatson/AskWatsonApplication.java
+++ src/main/java/com/apebble/askwatson/AskWatsonApplication.java
@@ -3,11 +3,13 @@ package com.apebble.askwatson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @EnableJpaAuditing
+@EnableSwagger2
 @SpringBootApplication
 public class AskWatsonApplication {
 
