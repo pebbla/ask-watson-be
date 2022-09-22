7dae43d86888da253dd6a194173fec8ec4167379
hanslelee <hssarah13@gmail.com>
2022-09-22 22:24:41 +0900

    회원 목록 조회 api 수정

--- src/main/java/com/apebble/askwatson/report/ReportJpaRepository.java
+++ src/main/java/com/apebble/askwatson/report/ReportJpaRepository.java
@@ -1,5 +1,6 @@
 package com.apebble.askwatson.report;
 
+import com.apebble.askwatson.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,4 +14,6 @@ public interface ReportJpaRepository extends JpaRepository<Report, Long> {
 
     @Query(value = "select r from Report r where r.handledYn =:handledYn and :searchWord is null or (r.reporter.userNickname like %:searchWord% or r.reportedUser.userNickname like %:searchWord% or r.content like %:searchWord% or r.review.content like %:searchWord% or r.review.theme.themeName like %:searchWord% or r.review.theme.cafe.cafeName like %:searchWord%)")
     List<Report> findReportsByHandledYnAndSearchWord(String searchWord, Boolean handledYn);
+
+    int countByReportedUser(User user);
 }
--- src/main/java/com/apebble/askwatson/review/ReviewJpaRepository.java
+++ src/main/java/com/apebble/askwatson/review/ReviewJpaRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.apebble.askwatson.user.User;
 
 public interface ReviewJpaRepository extends JpaRepository<Review, Long> {
-
     List<Review> findByUser(User user);
+
     List<Review> findByThemeId(Long themeId);
 
+    int countByUser(User user);
 }
