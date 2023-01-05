fdaf39f9736de9cdb478778f25b1907ce2cd8c35
hanslelee <hssarah13@gmail.com>
2023-01-05 23:04:23 +0900

    사용되지 않는 HeartRepositoryCustom 삭제

--- src/main/java/com/apebble/askwatson/check/CheckRepository.java
+++ src/main/java/com/apebble/askwatson/check/CheckRepository.java
@@ -1,6 +1,5 @@
 package com.apebble.askwatson.check;
 
-import com.apebble.askwatson.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,7 +7,5 @@ import java.util.Optional;
 public interface CheckRepository extends JpaRepository<Check, Long>, CheckRepositoryCustom {
 
     Optional<Check> findByUserIdAndThemeId(Long userId, Long themeId);
-
-    int countByUser(User user);
 
 }
--- src/main/java/com/apebble/askwatson/heart/HeartRepository.java
+++ src/main/java/com/apebble/askwatson/heart/HeartRepository.java
@@ -1,12 +1,7 @@
 package com.apebble.askwatson.heart;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HeartRepository extends JpaRepository<Heart, Long>, HeartRepositoryCustom {
-
-    Optional<Heart> findByUserIdAndThemeId(Long userId, Long themeId);
-
+public interface HeartRepository extends JpaRepository<Heart, Long> {
 }
 
